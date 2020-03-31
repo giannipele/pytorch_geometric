b3bdd974ab2930b6488fe02c721d0f02bd182b7b
Giovanni Pellegrini <gianni.pele92@gmail.com>
2020-03-31 15:37:04 +0200

    max pooling

--- examples/laf_sage.py
+++ examples/laf_sage.py
@@ -1,13 +1,14 @@
-import os
+import os.path as osp
 import torch
 import torch.nn.functional as F
-from torch_geometric.datasets import Planetoid
+from torch_geometric.datasets import Planetoid, Reddit, PPI
 import torch_geometric.transforms as T
 from torch_geometric.nn import SplineConv, SAGELafConv, SAGEConv
 import math
 import numpy as np
 import time
 from sklearn.metrics import classification_report, f1_score, accuracy_score
+from torch_geometric.data import NeighborSampler
 from torch.nn import Linear
 
 
@@ -21,9 +22,9 @@ class GraphSAGE(torch.nn.Module):
             self.convs.append(SAGEConv(hidden, hidden))
             #self.convs.append(SAGELafConv(hidden, hidden))
         #self.convn = SAGELafConv(hidden, dataset.num_classes)
-        self.convn = SAGEConv(hidden, hidden)
+        self.convn = SAGEConv(hidden, dataset.num_classes)
         #self.lin1 = Linear(hidden, hidden)
-        self.lin2 = Linear(hidden, dataset.num_classes)
+        #self.lin2 = Linear(hidden, dataset.num_classes)
 
     def reset_parameters(self):
         self.conv1.reset_parameters()
@@ -31,18 +32,18 @@ class GraphSAGE(torch.nn.Module):
             conv.reset_parameters()
         self.convn.reset_parameters()
         #self.lin1.reset_parameters()
-        self.lin2.reset_parameters()
+        #self.lin2.reset_parameters()
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = self.conv1(x, edge_index)
+        x = F.relu(self.conv1(x, edge_index))
         x = F.dropout(x, p=0.5, training=self.training)
         for conv in self.convs:
-            x = conv(x, edge_index)
-        x = self.convn(x, edge_index)
+            x = F.relu(conv(x, edge_index))
+        x = F.relu(self.convn(x, edge_index))
         #x = F.relu(self.lin1(x))
         #x = F.dropout(x, p=0.5, training=self.training)
-        x = self.lin2(x)
+        #x = self.lin2(x)
         return F.log_softmax(x, dim=-1)
 
     def __repr__(self):
@@ -72,17 +73,26 @@ def gen_folds(n_data, folds, seed):
         rest = val_mask + test_mask
         train_mask = ~rest
 
-        # print(val_idx,val_end)
-        # print(test_idx, test_end)
-        # print("TR:", train_mask)
-        # print("VL:", val_mask)
-        # print("TS:", test_mask)
-        # print("=============================")
-
         test_idx = test_idx + offset
         val_idx = 0 if val_idx + offset >= n_data else val_idx + offset
         yield (train_mask, val_mask, test_mask)
 
+
+def get_dataset(name):
+    if name.to_lower() == 'cora':
+        path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
+        dataset = Planetoid(path, name, T.NormalizeFeatures())
+        loader = NeighborSampler(dataset[0], size=[25, 10], num_hops=2, batch_size=1000,
+                                 shuffle=False, add_self_loops=True)
+        return dataset, loader
+    elif name.to_lower() == 'reddit':
+        path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
+        dataset = Reddit(path)
+        loader = NeighborSampler(dataset[0], size=[25, 10], num_hops=2, batch_size=1000,
+                                 shuffle=False, add_self_loops=True)
+        return dataset, loader
+    elif name.to_lower() == 'ppi':
+        pass
 
 def train(model, data, optimizer):
     model.train()
@@ -118,76 +128,50 @@ def test(model, data):
     return accs[0]
 
 
-def exp(exp_name, seed, style, shared):
+def exp(exp_name, dataset_name, seed, style, shared):
     torch.manual_seed(seed)
     res_dir = "results/"
+    start_time = time.time()
 
-    dataset = 'Cora'
-    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', dataset)
-    dataset = Planetoid(path, dataset, T.NormalizeFeatures())
-    data = dataset[0]
+    data = get_dataset('cora')
 
-    fold = 0
-    fold_accuracies = []
-    fold_f1 = []
     itr_time = []
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     data = data.to(device)
 
+    print("Loading {} dataset took {} s".format(dataset_name, time.time()-start_time))
+
+    model = GraphSAGE(dataset, num_layers=2, hidden=64).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.0001)
+    best_acc = 0
+    count = 0
+
     with open(res_dir + '{}.log'.format(exp_name), 'w') as flog:
-        start_time = time.time()
-        for tr_mask, vl_mask, ts_mask in gen_folds(data.x.shape[0], FOLDS, FOLDS_SEED):
-            fold += 1
-            print("FOLD:", fold)
-            flog.write("fold #{}\n".format(fold))
+        for epoch in range(1, EPOCH):
+            start_itr = time.time()
+            train(model, data, optimizer)
+            train_accs = validate(model, data)
+            log = 'Epoch: {:03d}, Train: {:.5f}, Validation: {:.5f}'
+            print(log.format(epoch, *train_accs))
+            log+='\n'
+            flog.write(log.format(epoch, *train_accs))
+            if train_accs[1] > best_acc:
+                best_acc = train_accs[1]
+                torch.save(model.state_dict(), res_dir + "{}.dth".format(exp_name))
+                print("Saving model at iteration {}".format(epoch))
+                count = 0
+            else:
+                count += 1
+            if count == 100:
+                break
 
-            data.train_mask = tr_mask
-            data.val_mask = vl_mask
-            data.test_mask = ts_mask
+            itr_time.append(time.time()-start_itr)
 
-            print('Train: {}'.format(torch.sum(data.train_mask)))
-            print('Validation: {}'.format(torch.sum(data.val_mask)))
-            print('Test: {}'.format(torch.sum(data.test_mask)))
-            flog.write('train: {}\n'.format(torch.sum(data.train_mask)))
-            flog.write('validation: {}\n'.format(torch.sum(data.val_mask)))
-            flog.write('test: {}\n'.format(torch.sum(data.test_mask)))
-
-            model = GraphSAGE(dataset, num_layers=2, hidden=64).to(device)
-            optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.0001)
-            best_acc = 0
-            count = 0
-            for epoch in range(1, EPOCH):
-                start_itr = time.time()
-                train(model, data, optimizer)
-                train_accs = validate(model, data)
-                log = 'Epoch: {:03d}, Train: {:.5f}, Validation: {:.5f}'
-                print(log.format(epoch, *train_accs))
-                log+='\n'
-                flog.write(log.format(epoch, *train_accs))
-                if train_accs[1] > best_acc:
-                    best_acc = train_accs[1]
-                    torch.save(model.state_dict(), res_dir + "{}.dth".format(exp_name))
-                    print("Saving model at iteration {}".format(epoch))
-                    count = 0
-                else:
-                    count += 1
-                if count == 100:
-                    break
-
-                itr_time.append(time.time()-start_itr)
-
-            model.load_state_dict(torch.load(res_dir + "{}.dth".format(exp_name)))
-            cr, test_acc, test_f1 = test(model, data)
-            print(cr)
-            flog.write(cr+"\n")
-            fold_accuracies.append(test_acc)
-            fold_f1.append(test_f1)
-        flog.write("----------\n")
-        flog.write("Avg Test Accuracy: {:.5f}\tVariance: {:.5f}\n".format(np.mean(fold_accuracies), np.var(fold_accuracies)))
-        print("Avg Test Accuracy: {:.5f}\tVariance: {:.5f}\n".format(np.mean(fold_accuracies), np.var(fold_accuracies)))
-        flog.write("Avg Test Micro-F1: {:.5f}\tVariance: {:.5f}\n".format(np.mean(fold_f1), np.var(fold_f1)))
-        print("Avg Test Micro-F1: {:.5f}\tVariance: {:.5f}\n".format(np.mean(fold_f1), np.var(fold_f1)))
+        model.load_state_dict(torch.load(res_dir + "{}.dth".format(exp_name)))
+        cr, test_acc, test_f1 = test(model, data)
+        print(cr)
+        flog.write(cr+"\n")
 
         stop_time = time.time() - start_time
         avg_itr_time = np.mean(itr_time)
@@ -200,11 +184,11 @@ def exp(exp_name, seed, style, shared):
 
 def main(exps):
     for e in exps:
-        exp(e['name'], e['seed'], e['style'], e['shared'])
+        exp(e['name'], e['dataset_name'], e['seed'], e['style'], e['shared'])
 
 
 if __name__ == '__main__':
-    exps = [{'name': 'laf_sage_cora2', "seed": 2603, "style":'frac', "shared":True},
+    exps = [{"name": 'laf_sage_cora2', "dataset_name": 'cora', "seed": 2603, "style": 'frac', "shared": True},
              ]
     main(exps)
 
--- torch_geometric/nn/conv/sage_conv.py
+++ torch_geometric/nn/conv/sage_conv.py
@@ -5,7 +5,6 @@ from torch.nn import Parameter
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.utils import add_remaining_self_loops
 from torch_geometric.laf import ElementAggregationLayer, FractionalElementAggregationLayer, ScatterAggregationLayer
-from torch_geometric.nn.pool import max_pool
 
 from itertools import repeat
 #import laf
@@ -42,10 +41,10 @@ class SAGEConv(MessagePassing):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.normalize = normalize
-        self.weight = Parameter(torch.Tensor(self.in_channels, out_channels))
+        self.weight = Parameter(torch.Tensor(in_channels, out_channels))
 
         if bias:
-            self.bias = Parameter(torch.Tensor(out_channels))
+            self.bias = Parameter(torch.Tensor(in_channels))
         else:
             self.register_parameter('bias', None)
 
@@ -61,14 +60,88 @@ class SAGEConv(MessagePassing):
             edge_index, edge_weight = add_remaining_self_loops(
                 edge_index, edge_weight, 1, x.size(0))
 
+        x = self.propagate(edge_index, size=size, x=x,
+                           edge_weight=edge_weight)
         if torch.is_tensor(x):
             x = torch.matmul(x, self.weight)
         else:
             x = (None if x[0] is None else torch.matmul(x[0], self.weight),
                  None if x[1] is None else torch.matmul(x[1], self.weight))
 
-        return self.propagate(edge_index, size=size, x=x,
+        return F.relu(x)
+
+    def message(self, x_j, edge_weight):
+        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
+
+    def update(self, aggr_out):
+        if self.bias is not None:
+            aggr_out = aggr_out + self.bias
+        if self.normalize:
+            aggr_out = F.normalize(aggr_out, p=2, dim=-1)
+        return aggr_out
+
+    def __repr__(self):
+        return '{}({}, {})'.format(self.__class__.__name__, self.in_channels,
+                                   self.out_channels)
+
+
+class SAGEConvCorrect(MessagePassing):
+    r"""The GraphSAGE operator from the `"Inductive Representation Learning on
+    Large Graphs" <https://arxiv.org/abs/1706.02216>`_ paper
+
+    .. math::
+        \mathbf{\hat{x}}_i &= \mathbf{\Theta} \cdot
+        \mathrm{mean}_{j \in \mathcal{N(i) \cup \{ i \}}}(\mathbf{x}_j)
+
+        \mathbf{x}^{\prime}_i &= \frac{\mathbf{\hat{x}}_i}
+        {\| \mathbf{\hat{x}}_i \|_2}.
+
+    Args:
+        in_channels (int): Size of each input sample.
+        out_channels (int): Size of each output sample.
+        normalize (bool, optional): If set to :obj:`True`, output features
+            will be :math:`\ell_2`-normalized. (default: :obj:`False`)
+        bias (bool, optional): If set to :obj:`False`, the layer will not learn
+            an additive bias. (default: :obj:`True`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch_geometric.nn.conv.MessagePassing`.
+    """
+
+    def __init__(self, in_channels, out_channels, normalize=False, bias=True,
+                 **kwargs):
+        super(SAGEConvCorrect, self).__init__(aggr='mean', **kwargs)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.normalize = normalize
+        self.weight = Parameter(torch.Tensor(in_channels, out_channels))
+
+        if bias:
+            self.bias = Parameter(torch.Tensor(in_channels))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        uniform(self.in_channels, self.weight)
+        uniform(self.in_channels, self.bias)
+
+    def forward(self, x, edge_index, edge_weight=None, size=None):
+        """"""
+        if size is None and torch.is_tensor(x):
+            edge_index, edge_weight = add_remaining_self_loops(
+                edge_index, edge_weight, 1, x.size(0))
+
+        x = self.propagate(edge_index, size=size, x=x,
                               edge_weight=edge_weight)
+        if torch.is_tensor(x):
+            x = torch.matmul(x, self.weight)
+        else:
+            x = (None if x[0] is None else torch.matmul(x[0], self.weight),
+                 None if x[1] is None else torch.matmul(x[1], self.weight))
+
+        return F.relu(x)
 
     def message(self, x_j, edge_weight):
         return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
@@ -139,14 +212,14 @@ class SAGELafConv(MessagePassing):
             edge_index, edge_weight = add_remaining_self_loops(
                 edge_index, edge_weight, 1, x.size(0))
 
+        x = self.propagate(edge_index, size=size, x=x,
+                              edge_weight=edge_weight)
         if torch.is_tensor(x):
             x = torch.matmul(x, self.weight)
         else:
             x = (None if x[0] is None else torch.matmul(x[0], self.weight),
                  None if x[1] is None else torch.matmul(x[1], self.weight))
-
-        return self.propagate(edge_index, size=size, x=x,
-                              edge_weight=edge_weight)
+        return x
 
     def message(self, x_j, edge_weight):
         return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
@@ -167,10 +240,10 @@ class SAGELafConv(MessagePassing):
         the :obj:`aggr` argument.
         """
 
-        out = max_pool(index,inputs)
-        #v_min = torch.min(inputs)
-        #inputs = inputs - v_min
-        #out = self.aggregation(inputs, index)
+        #out = max_pool(index,inputs)
+        v_min = torch.min(inputs)
+        inputs = inputs - v_min
+        out = self.aggregation(inputs, index)
         return out
 
 
